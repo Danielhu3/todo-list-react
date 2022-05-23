fab472b91f03f14051cbbb10d2ae5ff72df89338
Danielhu3 <danielhu3123@gmail.com>
2022-05-23 17:41:03 -0300

    Get todo list data from api

--- src/Components/BottomPart.js
+++ src/Components/BottomPart.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { ALL_GET } from '../Api'
+import useFetch from '../Hooks/useFetch'
 import BottomBar from './BottomBar'
 import Input from './Input'
 
 
 const BottomPart = ({darkMode}) => {
-  const comments = ['aaa','bbb','ccc','ddd','5','a']
+  const {data, request} = useFetch()
+
+  React.useEffect(()=>{
+    async function fetchList(){
+      const {url, options} = ALL_GET()
+      const {response, json} = await request(url, options)
+      console.log(response)
+      console.log(json)
+    }
+    
+    fetchList()
+  },[request])
+ 
+
   return (
     <>
          <div className={`bottom ${darkMode ? 'dark-mode' : ''}`}>
@@ -14,7 +29,9 @@ const BottomPart = ({darkMode}) => {
             </div>
             <div className='bottom-list-created'>
               {
-                comments.map((item) => <Input text={item} key={item} darkMode={darkMode}/>)
+                data ? data.map((item) => <Input text={item.text} key={item.id} darkMode={darkMode}/>)
+                : null
+                
               }
               <BottomBar darkMode={darkMode}/>
             </div>  
